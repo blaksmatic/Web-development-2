ba12f2f6048bf4402cd97e80f4981398c0fb85aa
blaksmatic <blaksmatic@gmail.com>
2016-03-01 17:57:20 +0000

    nearly finished

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,51 @@
 // Write any custom javascript functions here
+var $window = $(window)
+var $document = $(document)
+
+
+/*This function is resizing the navbar*/
+$document.ready(function () {
+    $window.scroll(function () {
+        var CurrentPosition = $document.scrollTop();
+        if (CurrentPosition > 30) {
+            $('.button').addClass('smallb');
+            $('#login-button').removeClass('success');
+        } else {
+            $('.button').removeClass('smallb');
+            $('#login-button').addClass('success');
+        }
+    });
+});
+
+
+/*This function is position indicator*/
+$document.ready(function () {
+    var homeDistance = $("#Home").offset().top;
+    var aboutDistance = $("#About").offset().top - $(window).height() / 2;
+    var ideaDistance = $("#Ideas").offset().top- $(window).height() / 2;
+    var introDistance = $("#Intro").offset().top - $(window).height() / 2;
+    var teamDistance = $("#Team").offset().top - $(window).height() / 2;
+    $window.scroll(function () {
+        var CurrentPosition = $document.scrollTop();
+        if (CurrentPosition > homeDistance && CurrentPosition <= aboutDistance)
+            $("#HomeButton").addClass("success");
+        else
+            $("#HomeButton").removeClass("success");
+        if (CurrentPosition > aboutDistance && CurrentPosition <= ideaDistance)
+            $("#AboutButton").addClass("success");
+        else
+            $("#AboutButton").removeClass("success");
+        if (CurrentPosition > ideaDistance && CurrentPosition <= introDistance)
+            $("#IdeasButton").addClass("success");
+        else
+            $("#IdeasButton").removeClass("success");
+        if (CurrentPosition > introDistance && CurrentPosition <= teamDistance)
+            $("#IntroButton").addClass("success");
+        else
+            $("#IntroButton").removeClass("success");
+        if (CurrentPosition > teamDistance)
+            $("#TeamButton").addClass("success");
+        else
+            $("#TeamButton").removeClass("success");
+    })
+});
